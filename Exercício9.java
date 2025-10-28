3e695bba553b6f07a6de148fff7f0dfe929d8242
gabrielcodeworks1-pixel <gabriel.code.works1@gmail.com>
2025-10-28 20:43:26 -0300

    Exercício9
    exercicio 9 finalizado.

--- Exercício9.java
+++ Exercício9.java
@@ -7,8 +7,18 @@
  *
  * @author Admin
  */
-public class Exercício7 {
+public class Exercicio9 {
     public static void main (String [] args) {
         
+        double valorCompra = 150.00; // Defina aqui o valor da compra
+        double valorFinal;
+
+        if (valorCompra > 100) {
+            valorFinal = valorCompra - (valorCompra * 0.10); // Desconto de 10%
+        } else {
+            valorFinal = valorCompra - (valorCompra * 0.05); // Desconto de 5%
+        }
+
+        System.out.println("Valor original: R$ " + valorCompra);
+        System.out.println("Valor final com desconto: R$ " + valorFinal);
     }
-}
